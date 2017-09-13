e68507cd0afe55877f6b43f164974c176639300b
Sourav Puri <souravpu@buffalo.edu>
2017-09-12 22:26:01 -0400

    TechBuddy Project

---
+++ app/src/main/java/com/login_signup_screendesign_demo/Main_Screen.java
@@ -0,0 +1,12 @@
+package com.login_signup_screendesign_demo;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by sourav on 9/12/17.
+ */
+
+public class Main_Screen extends Fragment {
+
+
+}
